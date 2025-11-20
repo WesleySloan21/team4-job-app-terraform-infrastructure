6e2478567b2b2e47e248127eb0878a73ae297577
WesleySloan21 <wesley.sloan@kainos.com>
2025-11-20 10:24:15 +0000

    fmt

--- main.tf
+++ main.tf
@@ -43,13 +43,13 @@ resource "azurerm_resource_group" "main" {
 
 # Create Azure Key Vault for managing secrets
 resource "azurerm_key_vault" "main" {
-  name                        = var.key_vault_name
-  location                    = azurerm_resource_group.main.location
-  resource_group_name         = azurerm_resource_group.main.name
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  sku_name                    = "standard"
-  enable_rbac_authorization   = true
-  
+  name                      = var.key_vault_name
+  location                  = azurerm_resource_group.main.location
+  resource_group_name       = azurerm_resource_group.main.name
+  tenant_id                 = data.azurerm_client_config.current.tenant_id
+  sku_name                  = "standard"
+  enable_rbac_authorization = true
+
   tags = var.tags
 }
 
